7b35eefefc7516b64628b58d3c68ebf2ed56b348
d3b0ra <deborac02@hotmail.com>
2022-06-21 14:15:37 -0400

    criação do projeto

---
+++ src/main/java/br/edu/ifms/salao/SalaoApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifms.salao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalaoApplication.class, args);
+	}
+
+}
